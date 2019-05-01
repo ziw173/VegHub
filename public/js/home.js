ee94a5891f281a392a220f494fb9688ff9abfaa8
aaerok <bbk001@ucsd.edu>
2019-05-01 02:37:15 -0700

    changed pic size

--- public/js/home.js
+++ public/js/home.js
@@ -8,7 +8,7 @@ $(document).ready(() => {
             success: (data) => {
                 if (data.pic) {
                     $('#food-title').html(data.name);
-                    $('#food-pic').attr('src', data.pic).attr('width', '300px');
+                    $('#food-pic').attr('src', data.pic).attr('width', '400px');
                 } else {
                     $('#food-title').html('Could not find the specified food.');
                     $('#food-pic').attr('src', '').attr('width', '0px');
