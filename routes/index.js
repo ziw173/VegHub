dd2fdc53a19b8150f0e6e68cb7844142db3cef46
Brandon Kim <bbk001@ucsd.edu>
2019-05-10 22:08:33 -0700

    post schema

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Post = require('../models/post');
+var fs = require('fs');
 
 const db = {
     'stuffed avocados' : { name: 'Stuffed Avocados', pic: 'pics/stuffed-avocado.jpg'},
@@ -9,7 +10,12 @@ const db = {
 }
 
 router.get('/', (req, res) => {
-    res.render('home');
+    Post.find({}).exec( (err, posts) => {
+        if (err) throw err;
+        res.render('home', {
+            posts: posts
+        });
+    });
 });
 
 router.get('/food', (req, res) => {
@@ -27,7 +33,36 @@ router.get('/food/:foodid', (req, res) => {
 });
 
 router.post('/postRecipe', (req,res) => {
-    console.log(req.body);
+    const name = req.body.name;
+    const body = req.body.message;
+    const recipe = req.body.instructions;
+    const tags = req.body.tags;
+    const likes = req.body.likes;
+    const comments = req.body.comments;
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const d = `${month}/${day}/${year}`;
+
+    const postData = {
+        name: name,
+        image: {
+            
+        },
+        body: body,
+        date: d,
+        tags: tags,
+        recipe: recipe,
+        likes: likes,
+        comments: comments
+    };
+
+    const newPost = new Post(postData);
+    newPost.save( (err) => {
+        if (err) throw err;
+        console.log("Posted.");
+    });
     res.redirect('/');
 });
 
